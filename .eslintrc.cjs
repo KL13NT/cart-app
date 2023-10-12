9bf7fd54c692df7a25025764056c68ad8ee37efa
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-10-12 13:37:29 +0300

    chore: inital commit
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+		"prettier",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "react", "react-refresh", "jsx-a11y"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"react/react-in-jsx-scope": 0,
+		"@typescript-eslint/consistent-type-imports": "error",
+	},
+};
---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		react({
+			babel: {
+				plugins:
+					mode === "production"
+						? [
+								[
+									"react-remove-properties",
+									{
+										properties: ["data-testid"], // Remove date-testid from production bundle
+									},
+								],
+						  ]
+						: [],
+			},
+		}),
+		tsconfigPaths(),
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ["react", "react-dom"],
+					zustand: ["zustand"],
+				},
+			},
+		},
+	},
+	test: {
+		environment: "jsdom",
+		globals: false,
+		setupFiles: ["./tests/setup.ts"],
+		coverage: {
+			enabled: true,
+			provider: "istanbul",
+			exclude: ["src/stores", "src/mocks"],
+		},
+		/**
+		 * Places all snapshots in the <root>/__snapshots__ folder
+		 */
+		resolveSnapshotPath: (testPath, snapExtension) => {
+			const relative = path.relative(
+				testPath + snapExtension,
+				path.resolve("./__snapshots__"),
+			);
+
+			const base = path.basename(testPath);
+
+			return path.resolve(testPath, relative, base + snapExtension);
+		},
+	},
+}));

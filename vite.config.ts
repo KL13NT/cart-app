37bce2ead791b265fecf06b50eeb4c9f80520a80
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-10-13 13:59:25 +0300

    fix: vite base on production
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-	base: mode === "production" ? "/" : "/cart-app/",
+	base: mode === "production" ? "/cart-app/" : "/",
 	plugins: [
 		react({
 			babel: {
